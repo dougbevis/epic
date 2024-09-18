b8c2a4ceb8e14c805cf7ff212e458e012083410a
dougbevis <douglas.e.bevis@gmail.com>
2024-09-17 22:43:55 -0400

    fix

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const scrapeSchedule = require('./scrapeSchedule');
 // Define the route for the home page
 app.get('/', async (req, res) => {
     const schedule = await scrapeSchedule();
-    res.render('index', { schedule });
+    res.render('index', { scrapeSchedule });
   });
 
 module.exports = router;
