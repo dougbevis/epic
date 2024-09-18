2e2e99458fdf3cd2da728b5891174ddd25652e7c
dougbevis <douglas.e.bevis@gmail.com>
2024-09-17 22:57:06 -0400

    logging

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,13 @@ const scrapeSchedule = require('../scrapeSchedule');
 
 // Define the route for the home page
 router.get('/', async (req, res) => {
-    const schedule = await scrapeSchedule();
-    res.render('index', { scrapeSchedule });
-  });
+    try {
+        const schedule = await scrapeSchedule();
+        res.render('index', { schedule }); // Pass the schedule to the view
+    } catch (error) {
+        console.error('Error fetching schedule:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 module.exports = router;
