412769002aba0fd511bccaf08380708f7d032605
dougbevis <douglas.e.bevis@gmail.com>
2024-09-17 23:09:40 -0400

    add logging

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const scrapeSchedule = require('../scrapeSchedule');
 // Define the route for the home page
 router.get('/', async (req, res) => {
     try {
+        console.log('Calling scrapeSchedule function...');
         const schedule = await scrapeSchedule();
+        console.log('Schedule fetched:', schedule);
         res.render('index', { schedule }); // Pass the schedule to the view
     } catch (error) {
         console.error('Error fetching schedule:', error);
